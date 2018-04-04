8d167f4264f2df23e395e0990e23e558e23390db
nkuhta <nicholas.kuhta@gmail.com>
2018-04-03 21:45:36 -0700

    1D

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Linear-Regression-Python
+# Linear Regression in Python 
+
+1.  **[1D Regression](https://github.com/nkuhta/Numpy-Stack/tree/master/Numpy)** - Analytical solution for 1D linear regression in vectorized form.
+
+##  References
+1.  **Linear Regression in Python** - Udemy   
+	https://www.udemy.com/data-science-linear-regression-in-python
---
+++ linear_1D.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load data
+x=[]
+y=[]
+
+for line in open('data_1d.csv'):
+    xi,yi = line.split(',')
+    #print(xi)
+    x.append(float(xi))
+    y.append(float(yi))
+
+# turn into numpy arrays
+x = np.array(x)
+y = np.array(y)
+
+#  plot raw data
+plt.scatter(x,y)
+#plt.show()
+
+# calculate a and b (vectorized)
+
+N = len(x)
+
+# sum(xi^2) - first demoninator term
+D1 = x.dot(x)
+# 1/N * [sum(xi)]^2
+D2 = x.mean()*x.sum()
+# calculate a and b (vectorized)
+a = (x.dot(y) - x.sum()*y.mean())/(D1-D2)
+b = (D1*y.mean() - x.mean()*x.dot(y))/(D1-D2)
+
+print('a = ',a)
+print('b = ',b)
+
+# best fit y=ax+b line 
+yhat = a*x+b
+
+plt.scatter(x,y)
+plt.plot(x,yhat)
+plt.show()
